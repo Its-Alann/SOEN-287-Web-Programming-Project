f6945f35458420f6918d137e137e2a067e6c7fbc
Michael <michael.totarella@mail.concordia.ca>
2021-03-03 12:47:18 -0500

    JavaScript done for all buttons done except "Add to cart" and back end buttons.

--- public/js/michael_buttons.js
+++ public/js/michael_buttons.js
@@ -1,34 +1,37 @@
 function moreDescription() {
     var description = document.getElementsByClassName("more-description")[0];
+    var button = document.getElementsByClassName("btn-more-descr")[0];
     if (description.style.display != "block") {          
         description.style.display = "block";
+        button.textContent = "Less Description";
     } else {
         description.style.display = "none";
+        button.textContent = "More Description";
     }
 }
 
-function plus(price) {
+function plus(price, product) {
     var btn = document.getElementsByClassName("quantity-plus")[0];
-    var qty = document.getElementsByClassName("quantity-input")[0];
+    var qty = document.getElementsByClassName("quantity-input-" + product)[0];
     qty.value ++;
 
-    productSubtotal(price);
+    productSubtotal(price, product);
 }
 
-function minus(price) {
+function minus(price, product) {
     var btn = document.getElementsByClassName("quantity-minus")[0];
-    var qty = document.getElementsByClassName("quantity-input")[0];
+    var qty = document.getElementsByClassName("quantity-input-" + product)[0];
     if (qty.value <= 1) {
         qty.value = 1;
     } else {
         qty.value --;
     }
 
-    productSubtotal(price);
+    productSubtotal(price, product);
 }
 
-function productSubtotal(price) {
-    var qty = document.getElementsByClassName("quantity-input")[0];
+function productSubtotal(price, product) {
+    var qty = document.getElementsByClassName("quantity-input-" + product)[0];
     var subtotal = qty.value * price;
-    document.getElementsByClassName("subtotal")[0].textContent = "(Subtotal: $" + subtotal.toFixed(2) + ")";
+    document.getElementsByClassName("subtotal-" + product)[0].textContent = "(Subtotal: $" + subtotal.toFixed(2) + ")";
 }
