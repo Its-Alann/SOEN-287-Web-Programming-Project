56492b7ca99dcc6fa1b0490755b15d13bf5d8006
Will <wt22@videotron.ca>
2021-03-14 15:39:33 -0400

    Finished amount counter implementation of bakery aisle and started more description button implementation

---
+++ public/js/bakery-aisle.js
@@ -0,0 +1,37 @@
+function increment(elementID){
+  var element = document.getElementById(elementID);
+  var amount = parseInt(element.value);
+  //console.log("amount:" + amount);
+
+  //Verify for the maximum number of items per order
+  if(amount <99){
+    element.value = (++amount).toString();
+  }
+}
+
+function decrement(elementID){
+  var element = document.getElementById(elementID);
+  var amount = parseInt(element.value);
+  //console.log("amount:" + amount);
+
+//Verify for the minimum number of items per order
+  if(amount >1){
+    element.value = (--amount).toString();
+  }
+}
+
+function updateValues(){
+  keyList = (Object.keys(sessionStorage));
+  console.log(keyList);
+}
+
+function showDescription(){
+  var description = document.getElementById("descriptionText");
+  if(descrption.style.display == "none"){
+    description.style.display = "block" ;
+  
+  }
+  else{
+    description.style.display = "none";
+  }
+}
