41e509df9c0ec9b13efcb21a45caf77bf6e63230
Jbkim2001 <78226746+Jbkim2001@users.noreply.github.com>
2021-04-18 19:48:08 -0400

    add/del new php

---
+++ public/php/add_del_order.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>McJawz | Order List </title>
+    <link rel="stylesheet" href="../../public/css/bootstrap.css">
+    <link rel="stylesheet" href="../../public/css/backstore.css">
+    <link rel="icon" href="../../images/favicon.ico" type="image/x-icon" />
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="../html/index.html">
+            <img style="margin-right: 5px;" class="icon-logo" src="../../images/mcJawz_logo_no_txt.png" width="40" height="40" alt="">
+            McJawz
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../html/index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../html/signup.html">Sign Up</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../html/login.html">Log in</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="page-name">
+        <div>
+           Order List
+        </div>
+    </div>
+
+
+    <!-- ADD AN ORDER -->
+    <div class="flex-wrapper">
+        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 align-self-center mx-auto mt-5  mb-5">
+          <div class="card text-center">
+                <div class="card-header bg-dark text-white">
+                    <h1>Create an order</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+        <?php
+            $xml = simplexml_load_file('order_info.xml');
+            if (isset($_POST['insert'])){
+                #$xml = simplexml_load_file('../../order_info.xml');
+                #$currentAmount = $xml->amount;
+                #$newOrder = $xml -> order_list-> addChild("order");
+                #$newUser = $xml ->addChild("user", $_POST['user']);
+                #$newOrderNum = $xml ->addChild("order_num", $_POST['order_num']);
+                #$newOrderList = $xml ->addChild("orderList", $_POST['orderList']);
+                #$xml->amount = intval($currentAmount)+1;
+                #$xml->asXML("../../order_info.xml");
+                
+                $xml = new DomDocument("1.0", "UTF-8");
+                $xml -> load('order_info.xml');
+                
+                $newUser = $_POST['user'];
+                $newOrderNum = $_POST['order_num'];
+                $newOrderList = $_POST['orderList'];
+                
+                $rootTag = $xml -> getElementsByTagname('order_list') -> item(0);
+                
+                $infoTag = $xml -> createElement("order");
+                $userTag = $xml -> createElement("user", $newUser);
+                $orderNumTag = $xml -> createElement("order_num", $newOrderNum);
+                $orderListTag = $xml -> createElement("orderList", $newOrderList);
+                
+                $infoTag ->appendChild($userTag);
+                $infoTag ->appendChild($orderNumTag);
+                $infoTag ->appendChild($orderListTag);
+                
+                $rootTag -> appendChild($infoTag);
+                $xml->save('order_info.xml');
+                
+            
+                //$newUser = $_COOKIE['order_user'];
+                //$newOrderNum = $_COOKIE['order_order_num'];
+                //$newOrderList = $_COOKIE['order_orderList'];
+
+                echo ("Your order has been successfully added!");
+            }
+        ?>
+            
+
+    <div class="card-body my-auto ">
+        <form method = "POST" action="add_order.php">
+            Order Info wished to be added</br>
+            User Name <input type = "text" name = "user"></br>
+            Order Number <input type = "text" name = "order_num"></br>
+            Order List <input type = "text" name = "orderList" class = "mt-5 mb-5"></br>
+            
+            <input type="submit" name = "insert" value = "Add" class="btn btn-primary mt-3 mb-3">
+            <button type="button"  class="btn btn-danger mt-3 mb-3">Reset</button>
+            
+        </form>
+    </div>
+
+
+    <!-- DELETE AN ORDER-->
+    <div class="flex-wrapper">
+        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 align-self-center mx-auto mt-5  mb-5">
+          <div class="card text-center">
+                <div class="card-header bg-dark text-white">
+                    <h1>Delete an order</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+            
+    <form method = "POST" action = "delete_order.php">
+        Order wished to be deleted </br>
+        Order Number <input type = "text" name = "order_num"></br>
+        <input type="submit" name = "delete" value = "Delete" class="btn btn-primary mt-3 mb-3">
+        <button type="button"  class="btn btn-danger mt-3 mb-3">Reset</button>
+    </form>
+        <?php
+            if(isset($_POST['delete'])){
+                $xml = new DomDocument();
+                $xml->load('order_info.xml');
+                
+                $orderNum = $_POST['order_num'];
+                
+                $xpath = new DOMXPath($xml);
+                
+                foreach($xpath -> query("/order_list/order[order_num = '$orderNum']") as $node){
+                    $node->parentNode->removeChild($node);
+                    
+                }
+                $xml -> formatoutput = true;
+                $xml -> save('order_info.xml');
+
+                echo ("Your order has been successfully deleted!");
+            
+            }
+        ?>
+
+
+    <div class="footer">
+        <div class="footer-item ml-3">
+            <a href="admin.html"><p>Admin</p></a>
+        </div>
+        <div class="footer-item mr-3">
+            <p class="font-italic"></p>
+        </div>
+    </div>
+</body>
+</html>
--- public/php/edit_order.php
+++ public/php/edit_order.php
@@ -1,132 +1,176 @@
-<?php
-    session_start();
-    $newUser = $_COOKIE['order_user'];
-    $newOrderNum = $_COOKIE['order_order_num'];
-    $newOrderList = $_COOKIE['order_orderList'];
-
-    if(isset($_POST['edit'])){
-        $xml = new DomDocument();
-        $xml->load('order_info.xml');
-        
-        #del order
-        $orderNum = $_POST['order_num'];
-        
-        $xpath = new DOMXPath($xml);
-        
-        foreach($xpath -> query("/order_list/order[order_num = '$orderNum']") as $node){
-            $node->parentNode->removeChild($node);
-            
-        }
-        $xml -> formatoutput = true;
-        $xml -> save('order_info.xml');
-        
-        #add order
-        $newUser = $_POST['user'];
-        $newOrderNum = $_POST['order_num'];
-        $newOrderList = $_POST['orderList'];
-        
-        $rootTag = $xml -> getElementsByTagname('order_list') -> item(0);
-        
-        $infoTag = $xml -> createElement("order");
-        $userTag = $xml -> createElement("user", $newUser);
-        $orderNumTag = $xml -> createElement("order_num", $newOrderNum);
-        $orderListTag = $xml -> createElement("orderList", $newOrderList);
-        
-        $infoTag ->appendChild($userTag);
-        $infoTag ->appendChild($orderNumTag);
-        $infoTag ->appendChild($orderListTag);
-        
-        $rootTag -> appendChild($infoTag);
-        $xml->save('order_info.xml');
-
-        setcookie("order_user", $newUser, time() + 86400, "/");
-        setcookie("order_order_num", $newOrderNum,time() + 86400, "/" );
-        setcookie("order_orderList", $newOrderList,time() + 86400, "/");
-            header("Location: ../../index.php");
-    
-        echo ("Your order has been successfully edited!");
-        echo ("Your username is " + $newUser + ".");
-        echo ("Your order number is " + $newOrderNum + ".");
-        echo ("Your new order is " + $newOrderList + ".");
-
-    }
-    
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>McJawz | Order List </title>
-    <link rel="stylesheet" href="../../public/css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="../../public/css/backstore.css">
+
     <link rel="icon" href="../../images/favicon.ico" type="image/x-icon" />
+
+    <title>McJawz | Edit Order</title>
 </head>
 <body>
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="../html/index.html">
-            <img style="margin-right: 5px;" class="icon-logo" src="../../images/mcJawz_logo_no_txt.png" width="40" height="40" alt="">
-            McJawz
-        </a>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="../html/index.html">Home</a>
+    <!--HEADER-->
+    <div class="header">
+        <nav class="header-nav">
+            <ul class="navbar navbar-left">
+                <li class="logo">
+                    <a class="mr-2" href="../../public/html/index.html" >
+                        <img src="../../images/mcJawz_logo_no_txt.png" alt="">
+                    </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../html/signup.html">Sign Up</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../html/login.html">Log in</a>
+                <li class="logo">
+                    <a href="../../public/html/index.html" >
+                        McJawz                    
+                    </a>
                 </li>
             </ul>
-        </div>
-    </nav>
+            <ul class="navbar navbar-right">
+                <li class="login-signup">
+                    <a href="../../public/html/login.html">
+                        Login
+                    </a>
+                </li>
+                <li class="link-sep">|</li>
+                <li class="login-signup">
+                    <a href="../../public/html/signup.html">
+                        Sign Up
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 
+    <!--PAGE NAME-->
     <div class="page-name">
         <div>
-           Order List
+            Edit Order
         </div>
     </div>
 
-
-
-    <div class="flex-wrapper">
-        <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 align-self-center mx-auto mt-5  mb-5">
-          <div class="card text-center">
-                <div class="card-header bg-dark text-white">
-                    <h1>Edit an order</h1>
-                </div>
-            </div>
-        </div>
+    <!--SUBHEADER-->
+    <div class="subheader">
+        <nav class="subheader-nav">
+            <ul class="navbar navbar-left">
+                <li class="aisle-link">
+                    <a href="../../public/html/aisles/bakery.html" >
+                        Bakery                    
+                    </a>
+                </li>
+                <li class="link-sep">|</li>
+                <li class="aisle-link">
+                    <a href="../../public/html/aisles/beverages-aisle.html">
+                        Beverages
+                    </a>
+                </li>
+                <li class="link-sep">|</li>
+                <li class="aisle-link">
+                    <a href="../../public/html/aisles/dairyandeggs-aisle.html">
+                        Dairy & Eggs
+                    </a>
+                </li>
+                <li class="link-sep">|</li>
+                <li class="aisle-link">
+                    <a href="../../public/html/aisles/fruitsandveg-aisle.html" >
+                        Fruits & Vegetables                    
+                    </a>
+                </li>
+                
+                <li class="link-sep">|</li>
+                <li class="aisle-link">
+                    <a href="../../public/html/aisles/poultry-aisle.html">
+                        Meat, Poultry & Fish
+                    </a>
+                </li>
+                <li class="link-sep">|</li>
+                <li class="aisle-link">
+                    <a href="../../public/html/aisles/snack_aisle.html">
+                        Snacks
+                    </a>
+                </li>
+            </ul>
+            <ul class="navbar navbar-right">
+                <li class="cart-link">
+                    <a href="../../public/html/cart.html" >
+                        <img class="cart-icon" src="../../../images/cart_transparent_black.png" alt="">
+                    </a>
+                </li>
+                <li class="cart-link">
+                    <a href="../../public/html/cart.html">Cart</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 
+    <!--EDIT ORDERS-->
     <form method = "POST" action = "edit_order.php">
         Please select order number wished to be edited<input type = "text" name = "order_num"></br>
         Please enter the user's name <input type = "text" name = "user"></br>
         Please enter the new order list <input type = "text" name = "orderList" class = "mt-5 mb-5"></br>
         <input type="submit" name = "edit" value = "Edit" class="btn btn-primary mt-3 mb-3">
         <button type="button"  class="btn btn-danger mt-3 mb-3">Reset</button>
+        <button type="submit" class="btn btn-blue shadow-none mt-5">Save</button>
     </form>
+
+        <?php
+            $newUser = $_COOKIE['order_user'];
+            $newOrderNum = $_COOKIE['order_order_num'];
+            $newOrderList = $_COOKIE['order_orderList'];
+
+            if(isset($_POST['edit'])){
+                $xml = new DomDocument();
+                $xml->load('order_info.xml');
+                
+                #del order
+                $orderNum = $_POST['order_num'];
+                
+                $xpath = new DOMXPath($xml);
+                
+                foreach($xpath -> query("/order_list/order[order_num = '$orderNum']") as $node){
+                    $node->parentNode->removeChild($node);
+                    
+                }
+                $xml -> formatoutput = true;
+                $xml -> save('order_info.xml');
+                
+                #add order
+                $newUser = $_POST['user'];
+                $newOrderNum = $_POST['order_num'];
+                $newOrderList = $_POST['orderList'];
+                
+                $rootTag = $xml -> getElementsByTagname('order_list') -> item(0);
+                
+                $infoTag = $xml -> createElement("order");
+                $userTag = $xml -> createElement("user", $newUser);
+                $orderNumTag = $xml -> createElement("order_num", $newOrderNum);
+                $orderListTag = $xml -> createElement("orderList", $newOrderList);
+                
+                $infoTag ->appendChild($userTag);
+                $infoTag ->appendChild($orderNumTag);
+                $infoTag ->appendChild($orderListTag);
+                
+                $rootTag -> appendChild($infoTag);
+                $xml->save('order_info.xml');
+
+                
+                echo ("Your order has been successfully edited!");
+                echo ("Your username is " + $newUser + ".");
+                echo ("Your order number is " + $newOrderNum + ".");
+                echo ("Your new order is " + $newOrderList + ".");
+
+            }
             
+        ?>
 
 
-
-
-    <div class="footer">
-        <div class="footer-item ml-3">
-            <a href="admin.html"><p>Admin</p></a>
-        </div>
-        <div class="footer-item mr-3">
-            <p class="font-italic"></p>
+    <!--FOOTER-->
+    <div class="footer mt-lg-5">
+        <div class="footer-item">
+            <a href="../../public/html/admin.html">
+                <p>Admin</p>
+            </a>
         </div>
     </div>
 </body>
